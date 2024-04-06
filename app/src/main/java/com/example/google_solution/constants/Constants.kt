78d4af068c07c6bf3a57f45f267252874b532431
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2024-04-06 12:00:23 +0530

    commit 18 ishant

--- app/src/main/java/com/example/google_solution/constants/Constants.kt
+++ app/src/main/java/com/example/google_solution/constants/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.google_solution.constants
 
 object Constants {
-    const val API_KEY_GEMINI = "AIzaSyAwdXOpsKqnsPXTXHy-gEy3oZgd5rEXBkA"
+    const val API_KEY_GEMINI = "AIzaSyB2w3LK2SccoC23tTFiRdkyv7umVucExkg"
     const val textModel = "gemini-pro"
     const val imageTextModel = "gemini-pro-vision"
     const val privacyPolicyViolationText = "Please send a message that adheres to our privacy and explicit content policies. \uD83C\uDF10✉\uFE0F\uD83D\uDE0A"
